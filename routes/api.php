0623f2c1084ef09bb013f6c0b39c7c5b6404a1e0
syayid <syayidsyayid262@gmail.com>
2026-05-04 21:48:25 +0700

    create api tenant

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\RoomController;
+use App\Http\Controllers\TenantController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,3 +9,4 @@ Route::get('/', function () {
 });
 
 Route::apiResource('rooms', RoomController::class);
+Route::apiResource('tenants', TenantController::class);
