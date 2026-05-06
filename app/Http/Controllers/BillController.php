949277af45ff76651c4463dec67278efe6cbe845
syayid <syayidsyayid262@gmail.com>
2026-05-06 23:10:57 +0700

    create controller bill

---
+++ app/Http/Controllers/BillController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Bill\StoreBillRequest;
+use App\Http\Requests\Bill\UpdateBillRequest;
+use App\Models\Bill;
+use App\Services\BillService;
+use Illuminate\Http\Request;
+
+class BillController extends Controller
+{
+    public function __construct(private BillService $service)
+    {
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json($this->service->getAll());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreBillRequest $request)
+    {
+        $bill = $this->service->store($request->validated());
+        return response()->json($bill, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateBillRequest $request, Bill $bill)
+    {
+        $bill = $this->service->update($bill, $request->validated());
+        return response()->json($bill);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Bill $bill)
+    {
+        $this->service->delete($bill);
+        return response()->noContent();
+    }
+}
