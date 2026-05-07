0b2a04e2cc84c25e801feddac7f6b6ce28286a74
syayid <syayidsyayid262@gmail.com>
2026-05-07 13:50:06 +0700

    create request store & update payment

---
+++ app/Http/Requests/Payment/StorePaymentRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Payment;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StorePaymentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'bill_id' => 'required|exists:bills,id',
+            'paid_amount' => 'required|date',
+            'amount' => 'required|numeric|min:0',
+            'method' => 'required|in:cash,transfer,ewallet',
+        ];
+    }
+}
