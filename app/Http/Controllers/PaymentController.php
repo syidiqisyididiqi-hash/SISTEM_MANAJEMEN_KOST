941b765496fbff19180412d7c51d31612f7b935a
syayid <syayidsyayid262@gmail.com>
2026-05-07 21:13:17 +0700

    create payment controller

---
+++ app/Http/Controllers/PaymentController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Exception;
+use App\Models\Payment;
+use App\Services\PaymentService;
+use App\Http\Requests\Payment\StorePaymentRequest;
+use App\Http\Requests\Payment\UpdatePaymentRequest;
+
+class PaymentController extends Controller
+{
+    public function __construct(private PaymentService $service)
+    {
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json($this->service->getAll());
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePaymentRequest $request)
+    {
+        try {
+            $payment = $this->service->store($request->validated());
+
+            return response()->json($payment, 201);
+
+        } catch (Exception $e) {
+
+            return response()->json([
+                'message' => $e->getMessage()
+            ], 400);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePaymentRequest $request, Payment $payment)
+    {
+        $payment = $this->service->update($payment, $request->validated());
+
+        return response()->json($payment);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Payment $payment)
+    {
+        $this->service->delete($payment);
+
+        return response()->noContent();
+    }
+}
