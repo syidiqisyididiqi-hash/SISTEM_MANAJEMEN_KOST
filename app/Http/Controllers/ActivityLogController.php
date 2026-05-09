a86cf6cfab24ff51b2b0ff4146af6b693f4fa100
syayid <syayidsyayid262@gmail.com>
2026-05-09 20:07:26 +0700

    create controler activty log

---
+++ app/Http/Controllers/ActivityLogController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ActivityLog\StoreActivityLogRequest;
+use App\Services\ActivityLogService;
+use Illuminate\Http\Request;
+
+class ActivityLogController extends Controller
+{
+    public function __construct(private ActivityLogService $service)
+    {
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json($this->service->getAll());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreActivityLogRequest $request)
+    {
+        $log = $this->service->create($request->validated()['description']);
+        return response()->json($log, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
