b696f59b72f6e81747587ebbff55c4caede519f7
syayid <syayidsyayid262@gmail.com>
2026-05-12 00:02:19 +0700

    create api user

--- routes/api.php
+++ routes/api.php
@@ -6,12 +6,14 @@ use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\RoomController;
 use App\Http\Controllers\RoomTenantController;
 use App\Http\Controllers\TenantController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::apiResource('users', UserController::class);
 Route::apiResource('rooms', RoomController::class);
 Route::apiResource('tenants', TenantController::class);
 Route::apiResource('room-tenants', RoomTenantController::class);
