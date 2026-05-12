d4ad5ad41cab4878287e4798f4640845667e595e
syayid <syayidsyayid262@gmail.com>
2026-05-12 21:07:49 +0700

    update user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -33,9 +33,10 @@ class UserController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show($id)
     {
-        //
+        $user = $this->service->findById($id);
+        return response()->json($user);
     }
 
     /**
