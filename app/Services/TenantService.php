94ce3e20a27aefc79fb89add98efac9df5fc4854
syayid <syayidsyayid262@gmail.com>
2026-05-14 18:39:20 +0700

    update tenant service show

--- app/Services/TenantService.php
+++ app/Services/TenantService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 use App\Models\Tenant;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class TenantService
 {
@@ -13,6 +14,17 @@ class TenantService
     public function store(array $data): Tenant
     {
         return Tenant::create($data);
+    }
+
+    public function show(int $id): Tenant
+    {
+        $tenant = Tenant::with('user')->find($id);
+
+        if (!$tenant) {
+            throw new ModelNotFoundException("Tenant not found");
+        }
+
+        return $tenant;
     }
 
     public function update(Tenant $tenant, array $data): Tenant
