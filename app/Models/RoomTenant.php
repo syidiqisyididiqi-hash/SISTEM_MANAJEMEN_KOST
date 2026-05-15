f810e1752aeef175627dedee2c10e0f4b5e0cd6f
syayid <syayidsyayid262@gmail.com>
2026-05-15 19:50:24 +0700

    menambahkan anotasi PHPDoc

--- app/Models/RoomTenant.php
+++ app/Models/RoomTenant.php
@@ -5,6 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property int $room_id
+ * @property int $tenant_id
+ * @property string|null $tenant_date
+ * @property \Illuminate\Support\Carbon|null $start_date
+ * @property \Illuminate\Support\Carbon|null $end_date
+ * @property string $status
+ * @property-read \App\Models\Room $room
+ * @property-read \App\Models\Tenant $tenant
+ */
 class RoomTenant extends Model
 {
     use HasFactory;
