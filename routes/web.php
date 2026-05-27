31530332d282efd97843862e7541fceb36687cc1
syayid <syayidsyayid262@gmail.com>
2026-05-27 21:41:48 +0700

    create route room

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::view('/tenant/dashboard', 'tenant.dashboard.index');
+Route::prefix('tenant')->name('tenant.')->group(function () {
+
+    Route::view('/dashboard', 'tenant.dashboard.index')
+        ->name('dashboard');
+
+    Route::view('/room', 'tenant.room.index')
+        ->name('room');
+
+    Route::view('/room/detail', 'tenant.room.detail')
+        ->name('room.detail');
+
+    Route::view('/payment', 'tenant.payment.index')
+        ->name('payment');
+
+});
